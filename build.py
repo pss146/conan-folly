474349731735403b850ae01d5dfdd0c7705c7643
pss146 <pss@inbox.ru>
2019-08-16 17:01:13 +0300

    Fix Windows build for Azure pipeline YML

--- build.py
+++ build.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     shared_option_name = False if platform.system() == "Windows"  or platform.system() == "Darwin" else "folly:shared"
     builder = build_template_default.get_builder(pure_c=False, shared_option_name=shared_option_name)
     if platform.system() == "Windows":
-        builder.remove_build_if(lambda build: "MD" in build.settings["compiler.runtime"])
+        builder.remove_build_if(lambda build: "MT" in build.settings["compiler.runtime"])
     builder.run()
